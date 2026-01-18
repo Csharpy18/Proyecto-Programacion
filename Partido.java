dabde6ea876b5c0bfb4e341f3f9ddeec1e25144d
Rodriiz0 <rodrigoriveco@gmail.com>
2026-01-17 19:52:30 -0600

    Creación clase partido

---
+++ Partido.java
@@ -0,0 +1,15 @@
+
+public class Partido
+{
+    private String nombre;
+    private String cedulaJuridica;
+    private String colores;
+    private String nombrePresidente;
+    private String correoElectronico;
+
+    public Partido()
+    {
+       
+    }
+
+}
