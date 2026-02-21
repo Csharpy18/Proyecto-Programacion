24a8d182d00761d7d6dae682f6bf6b7290053ccb
Rodriiz0 <rodrigoriveco@gmail.com>
2026-02-21 00:25:47 -0600

    Clase arbol completa

--- ArbolCandidatos.java
+++ ArbolCandidatos.java
@@ -1,17 +1,20 @@
 
-public class ArbolCandidatos
+public class ArbolCandidatos 
 {
     Nodo raiz;
-    
-    
+    //falta insercion busqueda y recorrido
     private class Nodo{
         private Candidato candidato;
-        private Nodo der;
-        private Nodo izq;
+        Nodo der;
+        Nodo izq;
         
         public Nodo(Candidato candidato){
             this.candidato = candidato;
             this.izq = this.der = null;
+        }
+        
+        public Candidato getCandidato(){
+            return candidato;
         }
         
         public String toString(){
@@ -22,7 +25,63 @@ public class ArbolCandidatos
     
     public ArbolCandidatos()
     {
-        // initialise instance variables
+       this.raiz = null;
+    }
+    
+
+    public Candidato buscar(String candidatoNombre){
+        Nodo actual = raiz;
+        boolean encontrado = false;
+        Candidato candidato = null;
         
+        while(actual != null && !encontrado){
+            int orden = (actual.getCandidato().getNombre().compareToIgnoreCase(candidatoNombre));
+            if(orden == 0){
+                candidato = actual.getCandidato();
+                encontrado = true;
+            }
+            else if(orden > 0){
+                actual = actual.der;
+            }
+            else{
+                actual = actual.izq;
+            }
+        }
+        if(!encontrado){
+            System.out.println("No se encontró el candidato.");
+        }
+        return candidato;
+    }
+    
+    public void recorrer(){
+        recorrerRec(raiz);
+    }
+    private void recorrerRec(Nodo nodo){
+        if (nodo != null){
+            recorrerRec(nodo.izq);
+            nodo.toString();
+            recorrerRec(nodo.der);
+        }
+    }
+    
+    public void insertar(Nodo nodo){
+        insertarRec(nodo, raiz);
+    }
+    private Nodo insertarRec(Nodo nodo, Nodo raiz){
+        Nodo nuevo = nodo;
+        Nodo actual = raiz;
+        if(raiz == null){
+            raiz = nuevo;
+        }
+        else{
+            if(nodo.getCandidato().getNombre().compareToIgnoreCase(nuevo.getCandidato().getNombre())>0){
+                actual.der = insertarRec(nodo, actual.der);
+            }
+            else{
+                nodo.izq = insertarRec(nodo, actual.izq);
+            }
+            
+        }
+        return nuevo;
     }
 }
